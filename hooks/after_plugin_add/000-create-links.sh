60ab83537f843902b5dd74b9308a741cec88d336
Miquel Martín <mmgadmin@MacMiquel.local>
2015-03-10 23:37:32 +0100

    Fix link issue

---
+++ hooks/after_plugin_add/000-create-links.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+
+ln -s -r plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/Versions/A/Headers/ plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/Headers
+
+
+ln -s -r plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/Versions/A/GoogleMobileAds plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/GoogleMobileAds
+
+
+ln -s -r plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/Versions/A/ plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/Versions/Current
