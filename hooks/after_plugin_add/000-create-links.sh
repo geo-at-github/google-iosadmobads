dd60557641aa4ceb4cf5e3c7ec2e267aa5ce1b83
geo-at-github <geo@geoathome.at>
2015-03-11 16:59:54 +0100

    test 4

--- hooks/after_plugin_add/000-create-links.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-cp -r $1/plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/Versions/A/Headers/ $1/plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/Headers
-cp -r $1/plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/Versions/A/GoogleMobileAds $1/plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/GoogleMobileAds
-cp -r $1/plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/Versions/A/ $1/plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/Versions/Current
